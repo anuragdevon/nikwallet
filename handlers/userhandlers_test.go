34659059e92973b668d83a245b7c3389577e1a60
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-17 16:19:00 +0530

    add handler test for duplicate user signup

--- handlers/userhandlers_test.go
+++ handlers/userhandlers_test.go
@@ -53,7 +53,50 @@ func TestUserHandlers(t *testing.T) {
 		assert.Contains(t, responseBody, "token")
 	})
 
-	t.Run("SignInHandler to return 200 StatusOk for successful user login", func(t *testing.T) {
+	t.Run("SignupHandler to return 500 InternalServerError for duplicate user entry", func(t *testing.T) {
+		// Create a user with the specified email ID using SignupHandler
+		newUser := map[string]interface{}{
+			"email_id":   "testhello456@example.com",
+			"password":   "password123",
+			"first_name": "Test",
+			"last_name":  "User",
+			"profession": "Engineer",
+		}
+
+		reqBody, err := json.Marshal(newUser)
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest("POST", "/signup", bytes.NewReader(reqBody))
+		assert.NoError(t, err)
+
+		recorder := httptest.NewRecorder()
+
+		http.HandlerFunc(userHandlers.SignupHandler).ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusCreated, recorder.Code)
+
+		duplicateUser := map[string]interface{}{
+			"email_id":   "testhello456@example.com",
+			"password":   "password456",
+			"first_name": "Duplicate",
+			"last_name":  "User",
+			"profession": "Designer",
+		}
+
+		reqBody, err = json.Marshal(duplicateUser)
+		assert.NoError(t, err)
+
+		req, err = http.NewRequest("POST", "/signup", bytes.NewReader(reqBody))
+		assert.NoError(t, err)
+
+		recorder = httptest.NewRecorder()
+
+		http.HandlerFunc(userHandlers.SignupHandler).ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusInternalServerError, recorder.Code)
+	})
+
+	t.Run("SignInHandler to return status 200 StatusOk for successful user login", func(t *testing.T) {
 		signinRequest := map[string]interface{}{
 			"email_id": "testhello321@example.com",
 			"password": "password123",
@@ -78,7 +121,7 @@ func TestUserHandlers(t *testing.T) {
 		assert.Contains(t, responseBody, "token")
 	})
 
-	t.Run("SigninHandler to return 401 Unauthorized for invalid user credentials", func(t *testing.T) {
+	t.Run("SigninHandler to return status 401 Unauthorized for invalid user credentials", func(t *testing.T) {
 		invalidSigninRequest := map[string]interface{}{
 			"email_id": "testhello321@example.com",
 			"password": "wrongpassword",
