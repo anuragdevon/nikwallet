e21cf2ac604b8582e0423d243d17ad62462a38f3
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-18 12:22:43 +0530

    add routing to transfer money

--- routers/walletrouters.go
+++ routers/walletrouters.go
@@ -14,6 +14,7 @@ func NewWalletRouter(handlers *handlers.WalletHandlers) *mux.Router {
 	router.HandleFunc("/create", handlers.CreateWalletHandler).Methods(http.MethodPost)
 	router.HandleFunc("/add", handlers.AddMoneyToWalletHandler).Methods(http.MethodPut)
 	router.HandleFunc("/withdraw", handlers.WithdrawMoneyFromWalletHandler).Methods(http.MethodPut)
+	router.HandleFunc("/transfer", handlers.WithdrawMoneyFromWalletHandler).Methods(http.MethodPut)
 
 	return router
 }
