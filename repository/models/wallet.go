6289bf0fca2fac566457163553a06f8d661a3309
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-30 13:46:01 +0530

    refactor: wallet model extension for wallet history for reference ids

--- repository/models/wallet.go
+++ repository/models/wallet.go
@@ -3,13 +3,14 @@ package models
 import (
 	"nikwallet/repository/money"
 	"time"
+	// pq "github.com/lib/pq"
 )
 
 type Wallet struct {
 	ID     int          `gorm:"column:id"`
 	UserID int          `gorm:"column:user_id"`
 	Money  *money.Money `gorm:"column:amount"`
-	// LedgerEntryIDs []int64      `gorm:"column:ledger_entry_ids"`
+	// LedgerEntryIDs pq.Int64Array `gorm:"type:integer[]"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
